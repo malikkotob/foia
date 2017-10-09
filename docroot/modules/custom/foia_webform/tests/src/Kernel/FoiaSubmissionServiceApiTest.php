c4c7ab135cb67a21f5aeee05f53d13ec474f2891
Dan Gurin <dan@dangur.com>
2017-10-09 13:55:05 -0400

    DOJ-121: Adds class to test submission to component.

---
+++ docroot/modules/custom/foia_webform/tests/src/Kernel/FoiaSubmissionServiceApiTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\Tests\foia_webform\Kernel;
+
+use Drupal\KernelTests\KernelTestBase;
+
+/**
+ * Class FoiaSubmissionServiceApiTest
+ *
+ * @package Drupal\Tests\foia_webform\Kernel
+ */
+class FoiaSubmissionServiceApiTest extends KernelTestBase {
+
+}
