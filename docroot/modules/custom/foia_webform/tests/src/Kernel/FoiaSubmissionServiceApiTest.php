13637b9e95b0f412173ac179bb013268a69c2856
Dan Gurin <dan@dangur.com>
2017-10-09 14:16:46 -0400

    DOJ-121: Adds webform from template.

--- docroot/modules/custom/foia_webform/tests/src/Kernel/FoiaSubmissionServiceApiTest.php
+++ docroot/modules/custom/foia_webform/tests/src/Kernel/FoiaSubmissionServiceApiTest.php
@@ -3,12 +3,61 @@
 namespace Drupal\Tests\foia_webform\Kernel;
 
 use Drupal\KernelTests\KernelTestBase;
+use Drupal\webform\Entity\Webform;
 
 /**
- * Class FoiaSubmissionServiceApiTest
+ * Class FoiaSubmissionServiceApiTest.
  *
  * @package Drupal\Tests\foia_webform\Kernel
  */
 class FoiaSubmissionServiceApiTest extends KernelTestBase {
+
+  /**
+   * Modules to install.
+   *
+   * @var array
+   */
+  public static $modules = [
+    'system',
+    'user',
+    'webform',
+    'webform_template',
+    'foia_webform',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    $this->installConfig(['system', 'webform', 'webform_template']);
+
+    // Create webform.
+    $webform = Webform::create(['id' => 'a_test_webform']);
+    $webform->set('foia_template', [
+      '#type' => 'checkbox',
+      '#title' => t("Use FOIA Agency template"),
+      '#disabled' => TRUE,
+      '#default_value' => 'foia_template',
+      '#value' => 'foia_template',
+    ]);
+    $webform->save();
+
+    // Create Agency Component.
+
+
+    // Create Webform Submission.
+
+
+  }
+
+  public function testsendSubmissionToComponent() {
+
+    $template = $webform->get('foia_template')->getValue();
+
+    $this->assertEquals('foia_template', $template)
+
+  }
 
 }
