2cf2594e7e1ccd3273f8376e400f09f8c3d162b3
Malik Kotob <malikkotob@users.noreply.github.com>
2017-10-10 00:28:13 -0400

    Malik: Progress on tests.

--- docroot/modules/custom/foia_webform/tests/src/Kernel/FoiaSubmissionServiceApiTest.php
+++ docroot/modules/custom/foia_webform/tests/src/Kernel/FoiaSubmissionServiceApiTest.php
@@ -3,7 +3,13 @@
 namespace Drupal\Tests\foia_webform\Kernel;
 
 use Drupal\KernelTests\KernelTestBase;
+use Drupal\node\NodeTypeInterface;
 use Drupal\webform\Entity\Webform;
+use Drupal\webform\Entity\WebformSubmission;
+use Drupal\field\Entity\FieldConfig;
+use Drupal\field\Entity\FieldStorageConfig;
+use Drupal\node\Entity\NodeType;
+use Drupal\node\Entity\Node;
 
 /**
  * Class FoiaSubmissionServiceApiTest.
@@ -40,7 +46,15 @@ class FoiaSubmissionServiceApiTest extends KernelTestBase {
    *
    * @var array
    */
-  public static $modules = ['webform_template', 'webform', 'system', 'user', 'foia_webform'];
+  public static $modules = [
+    'webform_template',
+    'webform',
+    'system',
+    'user',
+    'foia_webform',
+    'node',
+    'field'
+  ];
 
   /**
    * {@inheritdoc}
@@ -49,6 +63,8 @@ class FoiaSubmissionServiceApiTest extends KernelTestBase {
     parent::setUp();
     $this->installConfig(['webform', 'webform_template', 'foia_webform']);
     $this->installEntitySchema('webform_submission');
+    $this->installEntitySchema('user');
+    $this->installEntitySchema('node');
 
     // Creates webform and specifies to use the template fields.
     $webformWithTemplate = Webform::create(['id' => 'webform_with_template']);
@@ -59,6 +75,18 @@ class FoiaSubmissionServiceApiTest extends KernelTestBase {
     // Check creating a submission with default data.
     $webformSubmission = WebformSubmission::create(['webform_id' => $this->webform->id(), 'data' => ['custom' => 'value']]);
     $webformSubmission->save();
+    $this->webformSubmission = $webformSubmission;
+
+    // Adds Agency Component Content type.
+    $agencyComponentTypeDefinition = [
+      'type' => 'agency_component',
+      'name' => t('Agency Component'),
+      'description' => 'An agency component to which a request can be sent and which will be fulfilling requests.',
+    ];
+    $agencyComponentType = NodeType::create($agencyComponentTypeDefinition);
+    $agencyComponentType->save();
+    $this->addFieldsToComponentType($agencyComponentType);
+    $this->createAgencyComponentNode();
   }
 
   /**
@@ -66,6 +94,44 @@ class FoiaSubmissionServiceApiTest extends KernelTestBase {
    */
   public function testErrorResponseFromComponent() {
 
+  }
+
+  protected function addFieldsToComponentType(NodeTypeInterface $agencyComponentType) {
+    $this->addFieldToComponentType('field_request_submission_form', 'entity_reference', 'Request Submission Form');
+    $this->addFieldToComponentType('field_submission_api', 'uri', 'Field Submission Api');
+  }
+
+  protected function addFieldToComponentType($fieldName, $fieldType, $fieldLabel) {
+    $fieldStorage = FieldStorageConfig::create([
+      'field_name' => $fieldName,
+      'type' => $fieldType,
+      'entity_type' => 'node',
+    ]);
+    $fieldStorage->save();
+
+    $field = FieldConfig::create([
+      'field_name' => $fieldName,
+      'entity_type' => 'node',
+      'bundle' => 'agency_component',
+      'label' => $fieldLabel,
+    ]);
+    $field->save();
+  }
+
+  protected function createAgencyComponentNode() {
+    // Create an agency component entity.
+    /** @var \Drupal\node\NodeInterface $agencyComponent */
+    $agencyComponent = Node::create([
+      'type' => 'agency_component',
+      'title' => t('A Test Agency Component'),
+      'field_portal_submission_format' => 'api',
+      'field_submission_api' => 'http://atest.com',
+      'field_request_submission_form' => [
+        'target_id' => $this->webform->id()
+      ],
+    ]);
+    $agencyComponent->save();
+    $this->agencyComponent = $agencyComponent;
   }
 
 }
